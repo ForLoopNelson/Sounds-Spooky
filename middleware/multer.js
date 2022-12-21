5d7644e251e17f01e87c6fa4715841c58800a8d5
Cory Nelson <fullnelsontech@gmail.com>
2022-12-21 13:06:45 -0500

    change filetypes allowed to audio. need to test sample audio

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,8 +5,12 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname)
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".PNG") {
-      cb(new Error("File type is not supported"), false)
+    // if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".PNG") {
+    //   cb(new Error("File type is not supported"), false)
+    //   return
+    // }
+    if (ext !== ".mp3" && ext !== ".wav" && ext !== ".AAC" && ext !== ".AIFF") {
+      cb(new Error("File type is not supported!"), false)
       return
     }
     cb(null, true)
