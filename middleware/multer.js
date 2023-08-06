e3d59aea0bdef238b51076c8476910b71cf486dc
Cory Nelson <fullnelsontech@gmail.com>
2023-08-06 16:02:55 -0400

    remove commented out code

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,10 +5,6 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname)
-    // if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".PNG") {
-    //   cb(new Error("File type is not supported"), false)
-    //   return
-    // }
     if (ext !== ".mp3" && ext !== ".wav" && ext !== ".AAC" && ext !== ".AIFF") {
       req.flash('error','File type is not supported!')
       cb(null, false)
