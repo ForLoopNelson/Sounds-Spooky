bc092cde8ad48433d705bba16455dab6a3966842
Cory Nelson <fullnelsontech@gmail.com>
2023-08-18 16:51:23 -0400

    switched up ports to test deploy

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use("/post", postRoutes)
 app.use("/comment", commentRoutes)
 
 //Server Running
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 connectDB().then(() => {
 app.listen(
   PORT,
