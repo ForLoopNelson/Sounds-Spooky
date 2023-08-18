f41af9ab702608b7a83b8b04fe579fb9944d206b
Cory Nelson <fullnelsontech@gmail.com>
2023-08-18 15:38:29 -0400

    update mongoose pkg and add strict query and removed 2 option in conn

--- config/database.js
+++ config/database.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
+//test for deprecated warning
+mongoose.set("strictQuery", true)
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
+     
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
