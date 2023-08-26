89774354e3f28a7f9c4b16c13f6e9018e69799bf
Cory Nelson <fullnelsontech@gmail.com>
2023-08-26 17:55:36 -0400

    public folder for simple static js scripts

---
+++ public/js/audio.js
@@ -0,0 +1,21 @@
+// Get all audio elements
+const audios = document.querySelectorAll('audio')
+
+
+
+
+
+function pauseOtherAudios({target}) {
+  for (const audio of audios) {
+    if (audio !== target) {
+      audio.pause()
+  }
+}
+}
+
+// listen for play event
+for (const audio of audios) {
+  audio.addEventListener('play', pauseOtherAudios)
+}
+
+console.log('I ran')
