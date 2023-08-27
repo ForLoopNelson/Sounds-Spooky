cf4fc15c1854cb8f163f0214020dc89250e1bfde
Cory Nelson <fullnelsontech@gmail.com>
2023-08-27 12:40:54 -0400

    cleaned up and removed console log

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,21 +1,15 @@
 // Get all audio elements
 const audios = document.querySelectorAll('audio')
 
-
-
-
-
 function pauseOtherAudios({target}) {
   for (const audio of audios) {
     if (audio !== target) {
       audio.pause()
+    }
   }
 }
-}
-
 // listen for play event
 for (const audio of audios) {
   audio.addEventListener('play', pauseOtherAudios)
 }
 
-console.log('I ran')
