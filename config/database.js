920523b29e341abbd59e77f1af952dafb1ffe852
Cory Nelson <fullnelsontech@gmail.com>
2023-08-28 14:59:05 -0400

    add recommended bits to silence mongo deprecated errors

--- config/database.js
+++ config/database.js
@@ -5,7 +5,11 @@ mongoose.set("strictQuery", true)
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING);
+    const conn = await mongoose.connect(process.env.DB_STRING,
+    {useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+     useCreateIndex: true })
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
