f37198dce55db63a04473560b5836e4681b91ec2
Cory Nelson <fullnelsontech@gmail.com>
2023-09-12 13:39:10 -0400

    removed AJAX req as form is handled by controller and routes. just handle edit opening textarea

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -22,37 +22,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // Add an event listener to all edit forms
-  const editForms = document.querySelectorAll('.edit-form');
-  editForms.forEach(form => {
-    form.addEventListener('submit', async (event) => {
-      event.preventDefault();
-      const postId = form.getAttribute('data-postid');
-      const editedCaption = form.querySelector('textarea[name="editedCaption"]').value;
-
-      // Send an AJAX request to update the caption
-      try {
-        const response = await fetch(`/post/${postId}`, { // Change the URL to /editCaption
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ userId, editedCaption }), // Include postId in the request body
-        });
-
-        if (response.ok) {
-          // Update the displayed caption
-          caption.textContent = editedCaption;
-
-          // Hide the edit form
-          form.style.display = 'none';
-          caption.style.display = 'block';
-        } else {
-          console.error('Failed to update caption');
-        }
-      } catch (error) {
-        console.error(error);
-      }
     });
-  });
-});
