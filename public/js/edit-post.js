8c06d7819823d2028e8b84d86b43785cbe9827ff
Cory Nelson <fullnelsontech@gmail.com>
2023-09-20 14:00:56 -0400

    changed so clicking edit button closes textarea if open and opens it if closed

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,25 +1,30 @@
 
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
   // Add an event listener to all "Edit" buttons
   const editButtons = document.querySelectorAll('.edit-button');
   editButtons.forEach(button => {
     button.addEventListener('click', () => {
       const postId = button.getAttribute('data-postid');
-       const caption = document.querySelector(`.add-pad[data-postid="${postId}"]`);
       const editForm = document.querySelector(`.edit-form[data-postid="${postId}"]`);
+      const caption = document.querySelector(`.add-pad[data-postid="${postId}"]`);
       userId = editForm.getAttribute('data-userid'); // Retrieve userId
-      
 
-      if (caption) {
-        caption.style.display = 'none';
-      }
       if (editForm) {
-        editForm.style.display = 'block';
+        if (editForm.style.display === 'block') {
+          // If the edit form is currently open, close it
+          editForm.style.display = 'none';
+          if (caption) {
+            caption.style.display = 'block'; // Show caption
+          }
+        } else {
+          // If the edit form is currently closed, open it
+          editForm.style.display = 'block';
+          if (caption) {
+            caption.style.display = 'none'; // Hide caption
+          }
+        }
       }
     });
   });
-
-    });
+});
