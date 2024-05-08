e497bddc9d001d8bf43c72b4d0bbf59f05cc7f2f
Cory Nelson <fullnelsontech@gmail.com>
2024-05-08 13:22:04 -0400

    update model to get google user info

--- models/User.js
+++ models/User.js
@@ -5,6 +5,11 @@ const UserSchema = new mongoose.Schema({
   userName: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
+  googleId: { type: String, unique: true }, 
+  displayName: String, 
+  firstName: String,
+  lastName: String, 
+  image: String, 
 });
 
 // Password hash middleware.
