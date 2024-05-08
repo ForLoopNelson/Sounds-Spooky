1651a324f0d5313c1474bf361c131656d97b4d8a
Cory Nelson <fullnelsontech@gmail.com>
2024-05-08 13:21:37 -0400

    add google strategy for login

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy
 const mongoose = require("mongoose");
 const User = require("../models/User");
 
@@ -38,4 +39,40 @@ module.exports = function (passport) {
   passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => done(err, user));
   });
-};
+
+ 
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        }
+
+        try {
+          let user = await User.findOne({ googleId: profile.id })
+
+          if (user) {
+            done(null, user)
+          } else {
+            user = await User.create(newUser)
+            done(null, user)
+          }
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    )
+  )
+
+}
+;
