d68a7fdbf7cc80eeea57ecf9cc6f3753894a1f3f
Cory Nelson <fullnelsontech@gmail.com>
2025-05-19 12:15:32 -0400

    fix 'findOne doesn't accept callback' error and use async/await for postSignup

--- controllers/auth.js
+++ controllers/auth.js
@@ -97,7 +97,7 @@ exports.getSignup = (req, res) => {
   })
 }
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async (req, res, next) => {
   const validationErrors = []
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." })
@@ -122,32 +122,30 @@ exports.postSignup = (req, res, next) => {
     password: req.body.password,
   })
 
-  User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-    (err, existingUser) => {
-      if (err) {
-        return next(err)
-      }
-      if (existingUser) {
-        req.flash("errors", {
-          msg: "Account with that email address or username already exists.",
-        })
-        return res.redirect("../signup")
-      }
-      user.save((err) => {
-        if (err) {
-          return next(err)
-        }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err)
-          }
-          res.redirect("/profile")
-        })
-      })
+  try {
+    const existingUser = await User.findOne({
+      $or: [{ email: req.body.email }, { userName: req.body.userName }],
+    });
+
+    if (existingUser) {
+      req.flash("errors", {
+        msg: "Account with that email address or username already exists.",
+      });
+      return res.redirect("../signup");
     }
-  )
-}
+
+    await user.save();
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/profile");
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
 
 // Delete profile route
 // Controller to render the delete profile form
