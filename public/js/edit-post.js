cb82af3876ae155b736c7d3c5a2495fe69a1a1dd
Cory Nelson <fullnelsontech@gmail.com>
2025-05-27 11:59:28 -0400

    add logic to show user likes list

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,29 +1,35 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
-  // Add an event listener to all "Edit" buttons
+  // Handle Edit Button Toggle
   const editButtons = document.querySelectorAll('.edit-button');
   editButtons.forEach(button => {
     button.addEventListener('click', () => {
       const postId = button.getAttribute('data-postid');
       const editForm = document.querySelector(`.edit-form[data-postid="${postId}"]`);
       const caption = document.querySelector(`.add-pad[data-postid="${postId}"]`);
-      userId = editForm.getAttribute('data-userid'); // Retrieve userId
 
       if (editForm) {
-        if (editForm.style.display === 'block') {
-          // If the edit form is currently open, close it
-          editForm.style.display = 'none';
-          if (caption) {
-            caption.style.display = 'block'; // Show caption
-          }
-        } else {
-          // If the edit form is currently closed, open it
-          editForm.style.display = 'block';
-          if (caption) {
-            caption.style.display = 'none'; // Hide caption
-          }
+        const isVisible = editForm.style.display === 'block';
+        editForm.style.display = isVisible ? 'none' : 'block';
+        if (caption) {
+          caption.style.display = isVisible ? 'block' : 'none';
         }
+      }
+    });
+  });
+
+  // Handle "Liked By" Button Toggle
+  const likeHeaders = document.querySelectorAll('.liked-by');
+
+  likeHeaders.forEach(button => {
+    button.addEventListener('click', () => {
+      const postId = button.getAttribute('data-postid');
+      const showLikes = document.querySelector(`.likes-list[data-postid="${postId}"]`);
+      
+
+      if (showLikes) {
+        const isVisible = showLikes.style.display === 'block';
+        showLikes.style.display = isVisible ? 'none' : 'block';
+       
       }
     });
   });
