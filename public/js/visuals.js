e1e98ce4d59baeb064fbbfd492bebb34d82ee336
Cory Nelson <fullnelsontech@gmail.com>
2025-08-18 12:29:46 -0400

    add event listeners for user UI controls

--- public/js/visuals.js
+++ public/js/visuals.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const audio = document.getElementById('audio');
   const visualizerContainer = document.getElementById('visualizer');
+  const mirrorBtn = document.getElementById('mirrorToggle');
+  const radialBtn = document.getElementById('radialToggle');
 
   if (!audio || !visualizerContainer) return;
 
@@ -10,15 +12,38 @@ document.addEventListener('DOMContentLoaded', () => {
     source: audio,
     height: 200,
     mode: 4,
-    barSpace: 0.3,
+    barSpace: 0.6,
     gradient: 'prism',
     colorMode: 'bar-level',
     peakHoldTime: 800,
     frequencyScale: 'log',
+    roundBars: true,
     showScaleX: false,
     showScaleY: false,
+
+
+    
    
   });
+
+  //User changing UI settings logic
+
+document.getElementById('barSpace').addEventListener('input', (e) => {
+  audioMotion.barSpace = parseFloat(e.target.value);
+});
+
+// toggle mirror
+mirrorBtn.addEventListener('click', () => {
+  mirrorBtn.classList.toggle('active');
+  audioMotion.mirror = !audioMotion.mirror; // toggle setting
+});
+
+// toggle radial
+radialBtn.addEventListener('click', () => {
+  radialBtn.classList.toggle('active');
+  audioMotion.radial = !audioMotion.radial; 
+  
+});
 
 
 
@@ -36,4 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
     visualizerContainer.style.display = "none";
   });
 });
+
+
 
